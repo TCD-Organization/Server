b82856a679258b2b33a53ccd3e852e605d7ad0ee
Alexandre CHAMPAUD <alex.bis8@hotmail.fr>
2020-04-20 22:00:08 +0200

    Add Base of project

---
+++ src/test/java/fr/tcd/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tcd.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
