0c32decce6a51f7c4996712cc3e289dd29aa16e4
Alexandre CHAMPAUD <alex.bis8@hotmail.fr>
2020-06-08 09:03:41 +0200

    Remove DocumentRepositoryCustom

--- src/main/java/fr/tcd/server/document/DocumentRepository.java
+++ src/main/java/fr/tcd/server/document/DocumentRepository.java
@@ -1,31 +1,13 @@
 package fr.tcd.server.document;
 
-import fr.tcd.server.user.UserRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface DocumentRepository extends MongoRepository<DocumentModel, Long>, DocumentRepositoryCustom {
+public interface DocumentRepository extends MongoRepository<DocumentModel, Long> {
     boolean existsByOwnerAndChecksum(String owner, String checksum);
     Optional<DocumentModel> findById(String docID);
     Optional<List<DocumentModel>> findAllByOwner(String owner);
     //DocumentModel findById(String docId);
-}
-
-interface DocumentRepositoryCustom {
-    public void findByUser(String user);
-}
-
-class DocumentRepositoryImpl implements DocumentRepositoryCustom {
-    private final UserRepository userRepository;
-
-    DocumentRepositoryImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public void findByUser(String user) {
-
-    }
 }
