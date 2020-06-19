d5d945835cf680ed7d0fea4c1b446036741888c7
Alexandre CHAMPAUD <alex.bis8@hotmail.fr>
2020-06-19 10:17:52 +0200

    Update lasting_message type and verif

--- src/main/java/fr/tcd/server/analysis/dto/AnalysisProgressionDTO.java
+++ src/main/java/fr/tcd/server/analysis/dto/AnalysisProgressionDTO.java
@@ -16,19 +16,20 @@ public class AnalysisProgressionDTO {
     // TODO: @ValidStatus cannot be different than total_steps if status is "FINISHED"
     // TODO: @ValidStatus cannot be total_steps or above if status is not "FINISHED"
     @NotNull(message = "step_number must not be null")
-    @Range(min = 1)
+    @Range(min = 1, message = "step_number must at least be 1")
     private int step_number;
 
     // TODO: @ValidStatus cannot be below step_number
     @NotNull(message = "total_steps must not be null")
-    @Range(min = 1)
+    @Range(min = 1, message = "total_steps must at least be 1")
     private int total_steps;
 
     @NotEmpty(message = "step_name must not be empty")
     private String step_name;
 
-    @NotEmpty(message = "lasting_time must not be empty")
-    private Long lasting_time;
+    @NotNull
+    @Range(min = 0, message = "lasting_time must at least be 0")
+    private long lasting_time;
 
     // TODO: @ValidStatus cannot be empty if status is "FINISHED"
     private String result;
