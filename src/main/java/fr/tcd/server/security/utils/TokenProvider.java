35cf4c162e15a73d471946607779495ab450efec
Alexandre CHAMPAUD <alex.bis8@hotmail.fr>
2020-06-29 14:57:16 +0200

    Change tokenValidity expiration

--- src/main/java/fr/tcd/server/security/utils/TokenProvider.java
+++ src/main/java/fr/tcd/server/security/utils/TokenProvider.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
-    private final long tokenValidityInMilliseconds = Duration.ofMinutes(5).getSeconds() * 1000;
+    private final long tokenValidityInMilliseconds = Duration.ofHours(48).getSeconds() * 1000;
     private final byte[] secret;
 
     public TokenProvider(@Value("${security.token.secret}") CharSequence secret) {
